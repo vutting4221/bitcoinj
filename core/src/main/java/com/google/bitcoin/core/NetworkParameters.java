537d4b88c554ebd7ad87962329f69d4c082c91f1
Matt Corallo <git@bluematt.me>
2013-11-27 02:59:54 -0500

    Litecoin: Make bitcoin networkparameters constants protected

--- core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
+++ core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
@@ -111,9 +111,9 @@ public abstract class NetworkParameters implements Serializable {
         return genesisBlock;
     }
 
-    public static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 2 weeks per difficulty cycle, on average.
-    public static final int TARGET_SPACING = 10 * 60;  // 10 minutes per block.
-    public static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
+    protected static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 2 weeks per difficulty cycle, on average.
+    protected static final int TARGET_SPACING = 10 * 60;  // 10 minutes per block.
+    protected static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
     
     /**
      * Blocks with a timestamp after this should enforce BIP 16, aka "Pay to script hash". This BIP changed the
